c22ade40a4a56d3c551e271810544f1bf406328a
alaa <alaaadelanbr@gmail.com>
2026-08-23 04:53:47 +0300

    modified main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
@@ -7,6 +8,14 @@ from agent_core import ask_agent
 
 app = FastAPI(
     title="Syllabus & Exam Assistant API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
